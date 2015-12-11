8e3214a1ec2348497d8cad95e2f5ee081ee95f7f
AquaBytez <anddypena@outlook.com>
2015-12-10 19:57:10 -0500

    Fixed the InputManager + added method to reset the key states

--- DragonTale/src/tools/InputManager.java
+++ DragonTale/src/tools/InputManager.java
@@ -12,37 +12,32 @@ import javax.swing.KeyStroke;
 
 public class InputManager {
 
-    private InputMap im;
-    private ActionMap am;
     private static final int NUM_KEYS = 10;
     // Array that holds the ASCII values of the keys to check for input.
     private static int keyMappings[] = new int[NUM_KEYS];
+    // Array that works along the above array. Contains the key states.
     public static boolean keyState[] = new boolean[NUM_KEYS];
-    public static boolean prevKeyState[] = new boolean[NUM_KEYS];
 
     /**
      * The enum determines the order in which the keys are laid out.
      */
-    enum Keys {
+    public enum Keys {
 	UP, RIGHT, DOWN, LEFT, BUTTON1, BUTTON2, BUTTON3, BUTTON4, BUTTON5, BUTTON6
     };
 
-    // TODO Think of a way of getting away with this without a constructor.
-    /**
-     * Sample constructor call (assuming the caller extends some sort of JComponent): 
-     * InputManager input = new InputManager(getInputMap(WHEN_FOCUSED), getActionMap());
-     * @param im InputMap 
-     * @param am
-     */
-    public InputManager(InputMap im, ActionMap am) {
-	this.im = im;
-	this.am = am;
-    }
-
     /**
      * InputManager's equivalent of an init() method.
+     * 
+     * Sample call (assuming the caller extends some sort of JComponent):
+     * InputManager input = new InputManager(getInputMap(WHEN_FOCUSED),
+     * getActionMap());
+     * 
+     * @param im
+     *            InputMap
+     * @param am
+     *            ActionMap
      */
-    public void setKeyMappings() {
+    public static void setKeyMappings(InputMap im, ActionMap am) {
 	// TODO Load keymappings from a file
 	// --------Start of Hard-coded, temporary values-------------
 	keyMappings[0] = KeyEvent.VK_UP;
@@ -63,43 +58,54 @@ public class InputManager {
 
 		@Override
 		public void actionPerformed(ActionEvent e) {
-		    keySet(key.ordinal(), true);
-		    // Test code
-		    if (!isPressed(key.ordinal()))
-			System.out.println(key.name());
-		    // End of test code
+		    // When a key is pressed, set the pressed state of that key
+		    // to "true".
+		    keySet(keyMappings[key.ordinal()], true);
 		}
 	    });
 	}
     }
 
     /**
+     * This method resets all the key states back to false. This method must be
+     * called from within the game loop, otherwise, this class will produce
+     * "sticky" input.
+     */
+    public static void resetKeyStates() {
+	for (Keys i : Keys.values()) {
+	    keyState[i.ordinal()] = false;
+	}
+    }
+
+    /**
      * Sets the state of a key
      * 
-     * @param i
-     *            Key ASCCI value
+     * @param keyID
+     *            Key ASCCI value.
      * @param b
+     *            State to set the key to. "true" means that the key has been
+     *            pressed.
      */
-    private static void keySet(int i, boolean state) {
-	if (i == keyMappings[0])
+    private static void keySet(int keyID, boolean state) {
+	if (keyID == keyMappings[0])
 	    keyState[Keys.UP.ordinal()] = state;
-	if (i == keyMappings[1])
-	    keyState[Keys.LEFT.ordinal()] = state;
-	if (i == keyMappings[2])
-	    keyState[Keys.DOWN.ordinal()] = state;
-	if (i == keyMappings[3])
+	if (keyID == keyMappings[1])
 	    keyState[Keys.RIGHT.ordinal()] = state;
-	if (i == keyMappings[4])
+	if (keyID == keyMappings[2])
+	    keyState[Keys.DOWN.ordinal()] = state;
+	if (keyID == keyMappings[3])
+	    keyState[Keys.LEFT.ordinal()] = state;
+	if (keyID == keyMappings[4])
 	    keyState[Keys.BUTTON1.ordinal()] = state;
-	if (i == keyMappings[5])
+	if (keyID == keyMappings[5])
 	    keyState[Keys.BUTTON2.ordinal()] = state;
-	if (i == keyMappings[6])
+	if (keyID == keyMappings[6])
 	    keyState[Keys.BUTTON3.ordinal()] = state;
-	if (i == keyMappings[7])
+	if (keyID == keyMappings[7])
 	    keyState[Keys.BUTTON4.ordinal()] = state;
-	if (i == keyMappings[8])
+	if (keyID == keyMappings[8])
 	    keyState[Keys.BUTTON5.ordinal()] = state;
-	if (i == keyMappings[9])
+	if (keyID == keyMappings[9])
 	    keyState[Keys.BUTTON6.ordinal()] = state;
     }
 
@@ -110,8 +116,8 @@ public class InputManager {
      *            Key to check for its current state
      * @return Whether or not the key is pressed
      */
-    public static boolean isPressed(int i) {
-	return keyState[i] && !prevKeyState[i];
+    public static boolean isPressed(Keys key) {
+	return keyState[key.ordinal()];
     }
 
     /**
