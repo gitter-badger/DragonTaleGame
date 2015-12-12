7f3a0ee6c6f56a095d23cacd5e0412b1fcbb3ca1
AquaBytez <anddypena@outlook.com>
2015-12-12 16:38:39 -0500

    Updated the GameStateManager in preparation for using GameStates

--- DragonTale/src/gamestates/GameStateManager.java
+++ DragonTale/src/gamestates/GameStateManager.java
@@ -1,23 +1,48 @@
 package gamestates;
 
+import java.util.ArrayList;
+
 public class GameStateManager {
 
     private boolean isPaused = false;
+    private ArrayList<GameState> gameStates;
+    private GameStates currentState;
+    private GameStates previousState;
+
+    public static final int NUM_STATES = 1;
+
+    private enum GameStates {
+	MENU
+    };
 
     public GameStateManager() {
-	
+
+	isPaused = false;
+
+	gameStates = new ArrayList<>();
+	setState(GameStates.MENU);
     }
 
     private void loadState() {
 
     }
 
-    private void unloadState() {
-
+    private void unloadState(GameStates state) {
+	gameStates.remove(state.ordinal());
     }
 
-    public void setState(int state) {
+    public void setState(GameStates state) {
+	previousState = currentState;
+	unloadState(previousState);
+	currentState = state;
+	switch (state) {
+	case MENU:
+	    // TODO Make the MenuState compatible with the new changes.
+	    break;
+	default:
+	    break;
 
+	}
     }
 
     public void pause(boolean p) {
@@ -27,6 +52,7 @@ public class GameStateManager {
     public void tick() {
 	if (isPaused) {
 	    return;
-	}
+	} else
+	    gameStates.get(currentState.ordinal()).tick();
     }
 }
